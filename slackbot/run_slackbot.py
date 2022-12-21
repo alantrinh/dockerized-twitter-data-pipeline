6fd3ca7c8980cac52f7da8438fd2279f032bfe48
Alan <alan.l.trinh@gmail.com>
2022-12-21 23:59:17 +1100

    initial commit

---
+++ slackbot/run_slackbot.py
@@ -0,0 +1,28 @@
+from sqlalchemy import create_engine
+import pandas as pd
+import requests
+from credentials import SLACK_WEBHOOK_URL
+
+engine = create_engine('postgresql://postgres:1234@postgresdb:5432/twitter', echo=True)
+result = engine.execute('SELECT * FROM tweets ORDER BY created_at DESC').fetchone()._mapping
+
+webhook_url = SLACK_WEBHOOK_URL
+requests.post(url=webhook_url, json={
+    'blocks': [
+        {
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": f":fire:*_INCOMING MACHINE LEARNING TWEET_*:fire: \n\n*Posted at*: {result.created_at}  \n*Sentiment*: {result.sentiment}  \n*Tweet*:  \n\n_{result.text}_"
+			},
+			"accessory": {
+				"type": "image",
+				"image_url": "https://i.dailymail.co.uk/1s/2018/11/15/10/6216094-6390319-Michael_Rapaport_posted_a_short_clip_of_the_Chinchilla_Persian_k-m-12_1542278756274.jpg",
+				"alt_text": "alt text for image"
+			}
+		},
+    ]
+})
+
+#Tear down
+engine.execute('DROP TABLE IF EXISTS tweets')
